369661a497df0af3f84d43e8305c0b05788af849
Alex Li <alexlibaobao@gmail.com>
2024-10-21 22:53:17 -0400

    Added alarm sound for when time is up. The alarm sound pauses and resets to beginning when the stop button is pressed

--- HCI - Final Project/script.js
+++ HCI - Final Project/script.js
@@ -1,8 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
     let timerInterval;
     let totalTime;
+    const $timerDisplay = $('#timer-display');
+    const $startButton = $('#start-timer');
+    const $stopButton = $('#stop-timer');
+    const alarmSound = document.getElementById('alarm-sound');
 
-    $('#start-timer').on('click', function() {
+    $startButton.on('click', function () {
         const hours = parseInt($('#study-hours').val()) || 0;
         const minutes = parseInt($('#study-minutes').val()) || 0;
         const seconds = parseInt($('#study-seconds').val()) || 0;
@@ -12,32 +16,40 @@ $(document).ready(function() {
 
         if (totalTime > 0) {
             startCountdown();
+            $stopButton.removeClass('hidden');  // Show stop/reset button
         } else {
-            $('#timer-display').text("Please set a time :(");
+            $timerDisplay.text("Please set a time.");
         }
     });
 
-    $('#stop-timer').on('click', function() {
-        clearInterval(timerInterval);
-        $('#timer-display').text("00:00:00");
-        $('#study-hours').val('');
-        $('#study-minutes').val('');
-        $('#study-seconds').val('');
+    $stopButton.on('click', function () {
+        clearInterval(timerInterval);  // Stop the countdown
+        alarmSound.pause();  // Stop the sound
+        alarmSound.currentTime = 0;  // Reset sound to the beginning
+        resetTimerDisplay();
+        $stopButton.addClass('hidden');  // Hide stop/reset button
     });
 
     function startCountdown() {
-        clearInterval(timerInterval);
-
+        clearInterval(timerInterval);  // Clear any existing interval
         timerInterval = setInterval(() => {
-            if (totalTime >= 0) {
+            if (totalTime > 0) {
                 const display = formatTime(totalTime);
-                $('#timer-display').text(display);
+                $timerDisplay.text(display);
                 totalTime--;
             } else {
                 clearInterval(timerInterval);
-                $('#timer-display').text("Time's up!");
+                $timerDisplay.text("Time's up!");
+                alarmSound.play();  // Play the sound when time is up
             }
         }, 1000);
+    }
+
+    function resetTimerDisplay() {
+        $timerDisplay.text("00:00:00");
+        $('#study-hours').val('');
+        $('#study-minutes').val('');
+        $('#study-seconds').val('');
     }
 
     function formatTime(seconds) {
